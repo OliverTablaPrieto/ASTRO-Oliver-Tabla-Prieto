f8ee325fc0cfb0bcab2b85bd152467d41d35b895
OliverTablaPrieto <olivertabla10@gmail.com>
2026-02-22 01:08:52 +0100

    Missatge descriptiu del canvi

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://olivertablaprieto.github.io',
-  base: '/ASTRO-Oliver-Tabla-Prieto', 
+  base: '/',
 });
